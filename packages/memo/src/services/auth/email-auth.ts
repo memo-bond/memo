575e5c8f179f1d4583fdb5fc28696b30a93c89a3
lamle <lamle@gmx.com>
2022-02-12 12:04:45 +0000

    register success redirect to user page

--- packages/memo/src/services/auth/email-auth.ts
+++ packages/memo/src/services/auth/email-auth.ts
@@ -1,4 +1,3 @@
-import { debug } from '@/utils/log';
 import { createUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword, updateProfile, UserCredential } from '@firebase/auth';
 import { firebaseAuth } from '../firebase-service';
 
@@ -8,20 +7,13 @@ export const emailSignIn = async (email: string, password: string): Promise<User
 
 export const userRegister = async (registerParams: API.RegisterParams): Promise<UserCredential | string> => {
   try {
-    debug('1');
     const userCredential: UserCredential = await createUserWithEmailAndPassword(firebaseAuth, registerParams.email, registerParams.password);
-    debug('2');
-    debug(`New User Credential : ${JSON.stringify(userCredential)}`);
-    debug(`Send New User Email Verification ${userCredential.user.email}`);
     await sendEmailVerification(userCredential.user);
-    debug('3');
     updateProfile(userCredential.user, {
       displayName: registerParams.username
     })
-    debug('4');
     return userCredential;
   } catch (err: any) {
-    debug(`Error while create user: ${err.message}`);
     return err.message as string;
   }
 }
