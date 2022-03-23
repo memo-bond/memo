a344d15c9fdac227c98c5a721eeae19fe2a02b71
Lam Le <lamle@gmx.com>
2022-03-23 11:34:27 +0700

    quậy xíu nha thế anh :D

--- packages/memo/src/pages/Home/index.tsx
+++ packages/memo/src/pages/Home/index.tsx
@@ -2,9 +2,21 @@ import Footer from 'layout/Footer';
 import Header from 'layout/Header';
 import React, { memo } from 'react';
 import useStyles from './styles';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Rating from '@mui/material/Rating';
 
 // import Header from 'pages/Home/Header';
 // import Footer from 'pages/Home/Header';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 
 const HomePageComponent = () => {
@@ -12,8 +24,27 @@ const HomePageComponent = () => {
   return (
     <div className={css.homeRoot}>
       <div id="sectionBody" className={css.landingBody}>
-        <Header />
-        <Footer />
+        <Grid container spacing={2}>
+          <Grid item xs={24}>
+            <Item>
+              <Header />
+            </Item>
+          </Grid>
+          <Grid item xs={24}>
+            <Item>
+              <p>Rating Memo 💝</p>
+              <Rating
+                name="simple-controlled"
+                value={5}
+              />
+            </Item>
+          </Grid>
+          <Grid item xs={24}>
+            <Item>
+              <Footer />
+            </Item>
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
