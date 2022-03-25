3f6bc0abe29d75bf9157f91489d9bba7b62b3e41
tranlang <lang.tran@mservice.com.vn>
2022-03-25 14:37:45 +0700

    implement footer

--- packages/memo/src/layout/Footer/index.tsx
+++ packages/memo/src/layout/Footer/index.tsx
@@ -1,20 +1,112 @@
 import React from 'react';
 import {
-  Box
+  AppBar,
+  Box, Container, Grid, Toolbar, Typography
   // IconButton,
   // Popover,
 } from '@mui/material';
 
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import logo from 'assets/images/memo.png';
+import facebookIcon from 'assets/images/facebook.png';
+import instagramIcon from 'assets/images/instagram.png';
+import twitterIcon from 'assets/images/twitter.png';
+import youtubeIcon from 'assets/images/youtube.png';
+import githubIcon from 'assets/images/github.png';
+import gmailIcon from 'assets/images/gmail.png';
 import useStyles from './styles';
 
 const Footer = () => {
-  const classes = useStyles();
+  const css = useStyles();
 
   return (
-    <Box>
-    <a>Footer</a>
-    </Box>
+    <footer className={css.root}>
+      <Grid container className={css.footer}>
+        <Grid item className={css.logoContainer}>
+          <Link aria-label="homepage" className={css.logoLink} to={''}>
+            <img src={logo} alt="logo" className={css.logo} />
+          </Link>
+          <Typography component="h3" className={css.forMemoTxt}>
+            Memo
+          </Typography>
+        </Grid>
+
+      </Grid>
+      <Grid container className={css.footerContainer} spacing={4}>
+        <Grid item xs={3}>
+          <Typography component="h3" className={css.titleTxt}>
+            Services
+          </Typography>
+          <Typography component="h5" className={css.normalTxt}>
+            Salesforce Health Cloud
+          </Typography>
+          <Typography component="h5" className={css.normalTxt}>
+            Custom Software Engineering
+          </Typography>
+          <Typography component="h5" className={css.normalTxt}>
+            Data Management and Analytics
+          </Typography>
+          <Typography component="h5" className={css.normalTxt}>
+            Artificial Intelligence Enablement
+          </Typography>
+        </Grid>
+
+        <Grid item xs={3}>
+          <Typography component="h3" className={css.titleTxt}>
+            Solutions
+          </Typography>
+          <Typography component="h5" className={css.normalTxt}>
+            Laboratory Information Systems
+          </Typography>
+          <Typography component="h5" className={css.normalTxt}>
+            Custom Patient Cost Estimators
+          </Typography>
+          <Typography component="h5" className={css.normalTxt}>
+            Custom Patient Portals
+          </Typography>
+          <Typography component="h5" className={css.normalTxt}>
+            Custom Order and Results Portal
+          </Typography>
+          <Typography component="h5" className={css.normalTxt}>
+            Data-Driven Revenue Cycle Operations
+          </Typography>
+        </Grid>
+
+        <Grid item xs={3}>
+          <Typography component="h3" className={css.titleTxt}>
+            More
+          </Typography>
+          <Typography component="h5" className={css.normalTxt}>
+            Privacy Policy
+          </Typography>
+        </Grid>
+
+        <Grid item xs={3} className={css.contactUs}>
+          <Typography component="h3" className={css.titleTxt}>
+            Contact Us
+          </Typography>
+          <Link to={''}>
+            <img src={facebookIcon} alt="facebookIcon" className={css.iconContact} />
+          </Link>
+          <Link to={''} >
+            <img src={instagramIcon} alt="instagramIcon" className={css.iconContact} />
+          </Link>
+          <Link to={''}>
+            <img src={twitterIcon} alt="twitterIcon" className={css.iconContact} />
+          </Link>
+          <Link to={''}>
+            <img src={youtubeIcon} alt="youtubeIcon" className={css.iconContact} />
+          </Link>
+          <Link to={''}>
+            <img src={githubIcon} alt="githubIcon" className={css.iconContact} />
+          </Link>
+          <Link to={''}>
+            <img src={gmailIcon} alt="gmailIcon" className={css.iconContact} />
+          </Link>
+        </Grid>
+      </Grid>
+    </footer>
+
   );
 };
 
--- packages/memo/src/layout/Footer/styles.ts
+++ packages/memo/src/layout/Footer/styles.ts
@@ -1,9 +1,105 @@
 import { createStyles, makeStyles } from '@mui/styles';
 import { Theme } from '@mui/material';
+import { color, fontFamily, fontSize, letterSpacing, lineHeight } from '@mui/system';
 
-const styles = (theme: Theme) =>
+const styles = ({ spacing, breakpoints }: Theme) =>
   createStyles({
-  
+    root: {
+      backgroundColor: '#454545',
+      position: "static",
+    },
+    footer: {
+      display: 'flex',
+      textAlign: 'center',
+      alignContent: 'center',
+      justifyContent: 'space-between',
+      marginLeft: 20,
+      [breakpoints.down('sm')]: {
+        height: '52px',
+      },
+    },
+    logo: {
+      width: 50,
+      height: 50,
+      [breakpoints.up('md')]: {
+        width: '52px',
+        height: '52px',
+      },
+
+    },
+    logoContainer: {
+      display: 'flex',
+      alignItems: 'center',
+      marginTop: '30px',
+      marginLeft: spacing(2),
+    },
+
+    logoLink: {
+      display: 'flex',
+    },
+    forMemoTxt: {
+      fontSize: '20px',
+      fontWeight: 'bold',
+      color: '#FFF',
+      marginTop: '-2px',
+      fontFamily: 'Avenir-Heavy',
+      marginLeft: spacing(2),
+      [breakpoints.down('sm')]: {
+        fontSize: '16px',
+      },
+    },
+
+    footerContainer: {
+      marginTop: 5,
+      marginLeft: spacing(5),
+      display: 'flex',
+      [breakpoints.down('sm')]: {
+        fontSize: '20px',
+      },
+    },
+    titleTxt: {
+      color: '#6172a1',
+      fontSize: '17px',
+      fontWeight: 'bold',
+      marginTop: '15px',
+      marginBottom: '16px',
+      fontFamily: 'Avenir-Heavy',
+      letterSpacing: '-0.03px',
+      textAlign: 'left',
+      marginLeft: spacing(2),
+      [breakpoints.down('sm')]: {
+        fontSize: '16px',
+      },
+    },
+
+    normalTxt: {
+      color: '#bfc3e9',
+      fontSize: '15px',
+      fontFamily: 'Avenir-Medium',
+      letterSpacing: '-0.03px',
+      lineHeight: '33px',
+      marginLeft: spacing(2),
+      [breakpoints.down('sm')]: {
+        fontSize: '12px',
+      },
+
+    },
+
+    contactUs: {
+      [breakpoints.down('sm')]: {
+        marginLeft: 2,
+      },
+    },
+
+    iconContact: {
+      width: 25,
+      height: 25,
+      marginLeft: spacing(2),
+      [breakpoints.down('sm')]: {
+        width: 15,
+        height: 15,
+      },
+    },
   });
 
 const useStyles = makeStyles(styles, { index: 1, classNamePrefix: 'Footer' });
