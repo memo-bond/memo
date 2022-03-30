8c67f0b6cbae4d330cd0696e2b71bda21ca68514
tranlang <lang.tran@mservice.com.vn>
2022-03-30 17:11:25 +0700

    implement into section

--- packages/memo/src/components/IntroSection/index.tsx
+++ packages/memo/src/components/IntroSection/index.tsx
@@ -1,20 +1,56 @@
 import React from 'react';
 import {
-  Box
+  Box, Button, Grid, Link, Typography
   // IconButton,
   // Popover,
 } from '@mui/material';
 
 import { useHistory } from 'react-router-dom';
 import useStyles from './styles';
+import reactjs from 'assets/images/reactjs.png';
+import angular from 'assets/images/angular.png';
+import javascript from 'assets/images/javascript.png';
+
 
 const IntroSection = () => {
   const css = useStyles();
 
   return (
-    <Box className={css.root}>
-    <a>IntroSection</a>
-    </Box>
+    <Grid container className={css.root}>
+      <Grid item>
+        <Typography component="h1" className={css.titleTxt}>
+          Coding platform to empower engineers
+        </Typography>
+      </Grid>
+
+      <Grid item xs={4} className={css.containerDescription}>
+        <Typography component="h5" className={css.descriptionTxt}>
+          Create, share, and get feedback with collaborative sandboxes for rapid web development.
+        </Typography>
+      </Grid>
+
+      <Grid item className={css.containerIcon}>
+        <Link>
+          <img src={reactjs} alt="reactjs" className={css.iconFrameWork} />
+        </Link>
+        <Link>
+          <img src={angular} alt="angular" className={css.iconFrameWork} />
+        </Link>
+        <Link>
+          <img src={javascript} alt="javascript" className={css.iconFrameWork} />
+        </Link>
+      </Grid>
+
+      <Grid item className={css.containerBtn}>
+        <Button className={css.btnCodeing} variant="contained" size="medium"
+          onClick={() => {
+            alert('clicked');
+          }}>
+          #!# Start coding for free
+        </Button>
+      </Grid>
+    </Grid >
+
   );
 };
 
