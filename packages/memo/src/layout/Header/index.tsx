d5bbf9fb505d37ae6fd814ff25622428f646c1dc
Lam Le <lamle@gmx.com>
2023-01-22 21:04:24 +0700

    google authentication

--- packages/memo/src/layout/Header/index.tsx
+++ packages/memo/src/layout/Header/index.tsx
@@ -13,67 +13,74 @@ import {
 
 import logo from "assets/images/logo.svg";
 import useStyles from "./styles";
-import * as firebaseui from "firebaseui";
-import {
-  GoogleAuthProvider,
-  GithubAuthProvider,
-  EmailAuthProvider,
-  onAuthStateChanged,
-} from "@firebase/auth";
-import { firebaseAuth } from "../../index";
-import { useContext, useState } from "react";
+import { GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
+import { db, firebaseAuth } from "../../index";
+import { useEffect, useState } from "react";
 import { User } from "../../models/user";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import { authUser } from "App";
+import { atom, useRecoilValue, useSetRecoilState } from "recoil";
+import { collection, doc, getDocs, getDoc, setDoc } from "firebase/firestore";
 
 export function ListItemLink(props: ListItemProps<"a", { button?: true }>) {
   return <ListItem button component="a" {...props} />;
 }
+export const authUser = atom({
+  key: "authUser",
+  default: {} as User,
+});
 
-const ui = new firebaseui.auth.AuthUI(firebaseAuth);
 const Header = () => {
-  const setLoginUser = useSetRecoilState<User>(authUser);
-  const loginUser = useRecoilValue<User>(authUser);
-
+  const setLoginedUser = useSetRecoilState(authUser);
+  const loginedUser = useRecoilValue(authUser);
   const css = useStyles();
-  onAuthStateChanged(firebaseAuth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      const uid = user.uid;
-      console.log(uid);
-      setLoginUser({
-        email: user.email,
-        name: user.displayName,
-      });
-    } else {
+  const [loggedIn, setLoggedIn] = useState<boolean>();
+
+  useEffect(() => {
+    const login = localStorage.getItem("loggedIn");
+    if (login) {
+      setLoggedIn(true);
+      console.log("loggedIn ", loggedIn);
+      const user = localStorage.getItem("AuthUser");
+      setLoginedUser(JSON.parse(user!));
     }
-  });
-  const startUi = () => {
-    console.log("loginUser ", loginUser);
-    ui.start("#firebaseui-auth-container", {
-      signInOptions: [
-        {
-          provider: GithubAuthProvider.PROVIDER_ID,
-          signInMethod: GithubAuthProvider.GITHUB_SIGN_IN_METHOD,
-        },
-        {
-          provider: EmailAuthProvider.PROVIDER_ID,
-          signInMethod: EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
-        },
-        {
-          provider: GoogleAuthProvider.PROVIDER_ID,
-          signInMethod: GoogleAuthProvider.GOOGLE_SIGN_IN_METHOD,
-        },
-      ],
-      callbacks: {
-        signInSuccessWithAuthResult(result) {
-          return true;
-        },
-      },
-      signInFlow: "popup",
-      signInSuccessUrl: "/",
-    });
+  }, [loggedIn]);
+
+  const logout = () => {
+    // clear local storage
+    localStorage.removeItem("loggedIn");
+    localStorage.removeItem("AuthUser");
+    alert("Logout Successful");
+    location.href = "/";
+  };
+
+  const startUi = async () => {
+    signInWithPopup(firebaseAuth, new GoogleAuthProvider())
+      .then(async (result) => {
+        const user = result.user;
+        console.log("user ", user);
+        setLoggedIn(true);
+        const loginUser = {
+          uid: user.uid,
+          email: user.email,
+          name: user.displayName,
+          picture: user.photoURL,
+          username: user.email?.substring(0, user.email?.indexOf("@")),
+        };
+        setLoginedUser(loginUser);
+        localStorage.setItem("loggedIn", "true");
+        localStorage.setItem("AuthUser", JSON.stringify(loginUser));
+        const userRef = doc(db, "users", user.uid);
+        const userSnap = await getDoc(userRef);
+        if (userSnap.exists()) {
+          console.log("Document data:", userSnap.data());
+        } else {
+          // create new user
+          const usersRef = collection(db, "users");
+          await setDoc(doc(usersRef, user.uid), loginUser);
+        }
+      })
+      .catch((error: any) => {
+        console.log("login error ", error.message);
+      });
   };
   return (
     <header className={css.root}>
@@ -126,14 +133,37 @@ const Header = () => {
             </Hidden>
             <Hidden smDown>
               <Box display="flex" alignItems="center">
-                <Button
-                  className={css.signup}
-                  variant="text"
-                  size="small"
-                  onClick={startUi}
-                >
-                  Sign in/Sign Up
-                </Button>
+                {!loggedIn ? (
+                  <Button
+                    className={css.signup}
+                    variant="text"
+                    size="small"
+                    onClick={startUi}
+                  >
+                    Sign in
+                  </Button>
+                ) : (
+                  <>
+                    <Link href="/profile">
+                      <Button
+                        className={css.signup}
+                        variant="text"
+                        size="small"
+                      >
+                        {loginedUser.name}
+                      </Button>
+                    </Link>
+
+                    <Button
+                      className={css.signup}
+                      variant="text"
+                      size="small"
+                      onClick={logout}
+                    >
+                      Logout
+                    </Button>
+                  </>
+                )}
               </Box>
               <Box display="flex" alignItems="center">
                 <Link href="/code">
