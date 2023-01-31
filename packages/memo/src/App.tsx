9896ed081aee7aad77d82c16b019675517819389
Lam Le <lamle@gmx.com>
2023-01-31 09:16:15 +0700

    polishing

--- packages/memo/src/App.tsx
+++ packages/memo/src/App.tsx
@@ -1,17 +1,13 @@
-import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
 import { StylesProvider, createGenerateClassName } from "@mui/styles";
 import { SnackbarProvider } from "notistack";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient } from "react-query";
 import AdapterDayjs from "@mui/lab/AdapterDayjs";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import { ReactQueryDevtools } from "react-query/devtools";
 import theme from "theme";
 import AppRouter from "AppRouter";
-import { atom } from "recoil";
-import { User } from "models/user";
 
 const generateClassName = createGenerateClassName({
   seed: "Memo",
