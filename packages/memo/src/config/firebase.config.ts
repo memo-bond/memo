07cf6e6bad7bc93ddaec29f0a27a9902933ed655
Lam Le <lamle@gmx.com>
2023-02-01 14:46:17 +0700

    change firebase project

--- packages/memo/src/config/firebase.config.ts
+++ packages/memo/src/config/firebase.config.ts
@@ -1,8 +1,8 @@
 export default {
-  apiKey: "AIzaSyD3IzAH-FOh3_SDOBecJZtv4LRjwHfvc0s",
-  authDomain: "memo-9b895.firebaseapp.com",
-  projectId: "memo-9b895",
-  storageBucket: "memo-9b895.appspot.com",
-  messagingSenderId: "701798161487",
-  appId: "1:701798161487:web:48dd766a316c63911bde3a",
+  apiKey: "AIzaSyDsYlzbfWqMf7JeweKC6TJxY-m_RQ7nlcI",
+  authDomain: "memo-12c0a.firebaseapp.com",
+  projectId: "memo-12c0a",
+  storageBucket: "memo-12c0a.appspot.com",
+  messagingSenderId: "325646292483",
+  appId: "1:325646292483:web:1ab76dfa2bfcc812eda036",
 };
